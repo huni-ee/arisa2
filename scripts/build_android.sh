63d4f12e33c4526e47b180e23a0485c973bb5380
huni-ee <huni22ee@gmail.com>
2026-09-12 03:12:28 +0900

    arisa_control, arisa_control.ps1 추가, build_android.sh, release_android.sh 수정, README.md 업데이트

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -5,9 +5,11 @@ project_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$project_dir"
 
 cargo ndk --version >/dev/null
-cargo ndk -t arm64-v8a -t x86_64 --platform 23 build --release --locked
+cargo ndk -t armeabi-v7a -t arm64-v8a -t x86 -t x86_64 --platform 23 --link-builtins build --release --locked
 
 
 mkdir -p dist
+cp target/armv7-linux-android/release/arisa "dist/arisa-armeabi-v7a"
 cp target/aarch64-linux-android/release/arisa dist/arisa-arm64-v8a
+cp target/i686-linux-android/release/arisa dist/arisa-x86
 cp target/x86_64-linux-android/release/arisa dist/arisa-x86_64
--- scripts/release_android.sh
+++ scripts/release_android.sh
@@ -30,7 +30,7 @@ package_id="$(cargo pkgid)"
 version="${package_id##*@}"
 tag="v$version"
 
-if gh release view "$tag" --repo ye-seola/arisa2 >/dev/null 2>&1; then
+if gh release view "$tag" --repo huni-ee/arisa2 >/dev/null 2>&1; then
     echo "release $tag already exists" >&2
     exit 1
 fi
@@ -40,7 +40,7 @@ if git ls-remote --exit-code --tags origin "refs/tags/$tag" >/dev/null 2>&1; the
     exit 1
 fi
 
-cargo ndk -t arm64-v8a -t x86_64 --platform 23 build --release --locked
+cargo ndk -t armeabi-v7a -t arm64-v8a -t x86 -t x86_64 --platform 23 --link-builtins build --release --locked
 
 status="$(git status --porcelain --untracked-files=all)"
 if [[ -n "$status" ]]; then
@@ -50,16 +50,24 @@ if [[ -n "$status" ]]; then
 fi
 
 mkdir -p dist
+armeabi_v7a_artifact="dist/arisa-armeabi-v7a"
 arm64_artifact="dist/arisa-arm64-v8a"
+x86_artifact="dist/arisa-x86"
 x86_64_artifact="dist/arisa-x86_64"
+cp target/armv7-linux-androideabi/release/arisa "$armeabi_v7a_artifact"
 cp target/aarch64-linux-android/release/arisa "$arm64_artifact"
+cp target/i686-linux-android/release/arisa "$x86_artifact"
 cp target/x86_64-linux-android/release/arisa "$x86_64_artifact"
 
 gh release create "$tag" \
+    "$armeabi_v7a_artifact" \
     "$arm64_artifact" \
+    "$x86_artifact" \
     "$x86_64_artifact" \
+    "scripts/arisa_control" \
+    "scripts/arisa_control.ps1" \
     "prebuilts/fileprovider.apk" \
-    --repo ye-seola/arisa2 \
+    --repo huni-ee/arisa2 \
     --target "$commit" \
     --title "arisa $tag" \
     --generate-notes
